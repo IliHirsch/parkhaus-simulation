9b00ee103ad13b070d551eb4a43b3fbd94879850
Henrik <Henrik.Dubiel@outlook.de>
2026-02-24 21:28:28 +0100

    feat: add parking core API with pseudocode

---
+++ project_files/src/parking.c
@@ -0,0 +1,22 @@
+#include "parking.h"
+#include <stdlib.h>
+
+/* parking_init(...)
+ * PSEUDOCODE: ...
+ */
+
+/* parking_free(...)
+ * PSEUDOCODE: ...
+ */
+
+/* parking_find_free_slot(...)
+ * PSEUDOCODE: ...
+ */
+
+/* parking_handle_arrival(...)
+ * PSEUDOCODE: ...
+ */
+
+/* parking_process_departures(...)
+ * PSEUDOCODE: ...
+ */
