849f5ca4c8db33916e857dfd35151d61d859d692
Henrik <Henrik.Dubiel@outlook.de>
2026-02-24 21:25:33 +0100

    feat: add queue API prototypes with pseudocode

---
+++ project_files/src/queue.c
@@ -0,0 +1,26 @@
+#include "queue.h"
+#include <stdlib.h>
+
+/* queue_init(...)
+ * PSEUDOCODE: ...
+ */
+
+/* queue_free(...)
+ * PSEUDOCODE: ...
+ */
+
+/* queue_push(...)
+ * PSEUDOCODE: ...
+ */
+
+/* queue_pop(...)
+ * PSEUDOCODE: ...
+ */
+
+/* queue_size(...)
+ * PSEUDOCODE: ...
+ */
+
+/* queue_is_empty(...)
+ * PSEUDOCODE: ...
+ */
