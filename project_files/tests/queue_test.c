9cd14d937ce711aea690a4ddadac5beff7ac7d5c
Ilias Hirschmann <253711797+IliHirsch@users.noreply.github.com>
2026-03-13 21:57:41 +0100

    grobe Stuktur Queue_test

--- project_files/tests/queue_test.c
+++ project_files/tests/queue_test.c
@@ -5,3 +5,114 @@
 #include"types.h"
 #include"queue.h"
 
+/**
+ * @brief Hilfsfunktion zum Erzeugen eines Test-Fahrzeugs.
+ *
+ * @param[in] id Fahrzeug-ID.
+ * @param[in] restparkdauer Restparkdauer.
+ * @param[in] ankunftszeit Ankunftszeit.
+ * @return Erzeugtes Vehicle.
+ */
+static Vehicle make_vehicle(unsigned int id, int restparkdauer, int ankunftszeit)
+{
+    Vehicle v;
+    v.id = id;
+    v.restparkdauer = restparkdauer;
+    v.ankunftszeit = ankunftszeit;
+    return v;
+}
+
+
+/**
+ * @brief Prüft, dass queue_init eine Queue korrekt leer initialisiert.
+ */
+static void test_queue_init_initializes_empty_queue(void)
+{
+}
+
+/**
+ * @brief Prüft, dass queue_init(NULL) keinen Absturz verursacht.
+ */
+static void test_queue_init_with_null_does_not_crash(void)
+{
+}
+
+/**
+ * @brief Prüft, dass queue_free auf leerer Queue den Zustand korrekt belässt.
+ */
+static void test_queue_free_on_empty_queue_resets_state(void)
+{
+}
+
+/**
+ * @brief Prüft, dass queue_free alle Elemente einer befüllten Queue entfernt.
+ */
+static void test_queue_free_on_filled_queue_clears_all_nodes(void)
+{
+}
+
+
+
+/**
+ * @brief Prüft, dass queue_push auf leerer Queue das erste Element korrekt einfügt.
+ */
+static void test_queue_push_on_empty_queue_inserts_first_element(void)
+{
+}
+
+/**
+ * @brief Prüft, dass queue_push auf nicht-leerer Queue korrekt hinten anhängt.
+ */
+static void test_queue_push_on_non_empty_queue_appends_to_tail(void)
+{
+}
+
+
+/**
+ * @brief Prüft, dass queue_pop das erste Element im FIFO-Sinn entnimmt.
+ */
+static void test_queue_pop_returns_first_element_fifo(void)
+{
+}
+
+/**
+ * @brief Prüft, dass queue_pop auf leerer Queue false liefert.
+ */
+static void test_queue_pop_on_empty_queue_returns_false(void)
+{
+
+}
+
+/**
+ * @brief Prüft, dass queue_size bei leerer Queue 0 liefert.
+ */
+static void test_queue_size_of_empty_queue_is_zero(void)
+{
+}
+
+/**
+ * @brief Prüft, dass queue_size Push- und Pop-Operationen korrekt verfolgt.
+ */
+static void test_queue_size_tracks_push_and_pop_operations(void)
+{
+}
+
+
+/**
+ * @brief Prüft, dass queue_is_empty bei leerer Queue true liefert.
+ */
+static void test_queue_is_empty_returns_true_for_empty_queue(void)
+{
+}
+
+/**
+ * @brief Prüft, dass queue_is_empty nach einem Push false liefert.
+ */
+static void test_queue_is_empty_returns_false_after_push(void)
+{
+}
+
+int main(void)
+{
+    return 0;
+}
