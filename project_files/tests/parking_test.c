eff57b779f1f360d87d3489584a2ae509f398773
Ilias Hirschmann <253711797+IliHirsch@users.noreply.github.com>
2026-03-18 15:53:03 +0100

    hilfsfunktionen

---
+++ project_files/tests/parking_test.c
@@ -0,0 +1,25 @@
+#include<assert.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+#include "../include/parking.h"
+#include "../include/queue.h"
+#include "../include/stats.h"
+#include "../include/rng.h"
+#include "../include/types.h"
+#include "../include/config.h"
+
+static Vehicle make_vehicle(unsigned int id, int restparkdauer, int ankunftszeit)
+{}
+
+static SimConfig make_config(size_t kapazitaet, int max_parkdauer)
+{}
+
+
+
+int main (){
+
+
+    printf("Alle Parking-Tests erfolgreich.\n");
+    return 0;
+}
