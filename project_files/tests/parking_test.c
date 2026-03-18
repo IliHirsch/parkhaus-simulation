06cb5626f994913b635bc14e0eca022e732dcafa
Ilias Hirschmann <253711797+IliHirsch@users.noreply.github.com>
2026-03-18 15:57:21 +0100

    hilfsfunktionen ausgefüllt

--- project_files/tests/parking_test.c
+++ project_files/tests/parking_test.c
@@ -9,11 +9,31 @@
 #include "../include/types.h"
 #include "../include/config.h"
 
+/**
+ * @brief Hilfsfunktion zum Erzeugen eines Fahrzeugs.
+ */
 static Vehicle make_vehicle(unsigned int id, int restparkdauer, int ankunftszeit)
-{}
+{
+    Vehicle v;
+    v.id = id;
+    v.restparkdauer = restparkdauer;
+    v.ankunftszeit = ankunftszeit;
+    return v;
+}
 
+/**
+ * @brief Hilfsfunktion zum Erzeugen einer gültigen Test-Konfiguration.
+ */
 static SimConfig make_config(size_t kapazitaet, int max_parkdauer)
-{}
+{
+    SimConfig cfg;
+    cfg.anzahl_stellplaetze = kapazitaet;
+    cfg.max_parkdauer = max_parkdauer;
+    cfg.sim_dauer_zeitschritte = 10;
+    cfg.ankunft_wahrscheinlichkeit_prozent = 50;
+    cfg.seed = 123U;
+    return cfg;
+}
 
 
 
